060a956868ea22c638f4dca1e9229c5cd906c9c0
Michael Santana <michael.santana@dersalvador.com>
2019-09-06 23:34:55 +0200

    fixed wrapper script

--- knativeHeartBeatsSample.sh
+++ knativeHeartBeatsSample.sh
@@ -6,26 +6,26 @@ cleanup() {
 }
 trap 'cleanup' SIGTERM SIGINT SIGFPE 
 shopt -u extglob; set +H
-. bashrc_alias 2>&1 >/dev/null
 kubectx ${1:-minikube}
-echo "Usage: $0 KO_DOCKER_REPO (ko.local|gcr.io/plucky-door-2002008)" 
+echo "Usage: $0 KO_DOCKER_REPO [ko.local|default:=gcr.io/plucky-door-2002008]" 
 KO_DOCKER_REPO=${1:-grc.io/plucky-door-200208}
+mkdir -p ~/go/src/knative.dev/
 cd ~/go/src/knative.dev/
 export PROJECT_ID=$(gcloud config get-value core/project)
 export KO_DOCKER_REPO="gcr.io/${PROJECT_ID}"
 GOPATH_ORG=$GOPATH
-export GOPATH=/Users/michaelmellouk/go:/Users/michaelmellouk/go/src/knative.dev/eventing-contrib
+export GOPATH=$GOPATH:~/go/src/knative.dev/eventing-contrib
 [ ! -d eventing-contrib ] && git clone https://github.com/knative/eventing-contrib.git
 cd eventing-contrib
 git pull
-
 cd ~/go/src/knative.dev/eventing-contrib/cmd/heartbeats
-#echo ko resolve -f heartbeats-sender.yaml heartbeats-receiver.yaml
-#ko resolve -P -f heartbeats-sender.yaml 
-#ko resolve -f heartbeats-receiver.yaml
-# ko apply -L -f config/
-#cat ../../cmd/heartbeats/main.go
-#ko publish ../../cmd/heartbeats/main.go
-ko publish --local .
+go get github.com/google/ko/cmd/ko
+sleep 2
+# go get -u github.com/google/ko/cmd/ko
+ko publish .
 sleep 5
+echo heartbeat source logs
+kubectl logs --tail=50 -l eventing.knative.dev/source=heartbeats-sender 
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo heartbeat receiver logs
 kubectl logs --tail=50 -l serving.knative.dev/service=heartbeats-receiver -c user-container -f
